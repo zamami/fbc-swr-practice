065ce052007f21fa8611165f7e3692cd76bc4181
zamami <s1413115@gmail.com>
2025-02-06 18:59:13 +0900

    SWRの使用。 errorおよびisLoadingの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useSWR from "swr";
 import "./App.css";
+
+const headers = { Accept: "application/json" };
+const fetcher = async (url) => {
+  const res = await fetch(url, { headers });
+
+  if (!res.ok){
+    const error = new Error("An error occurred while fetching the data.");
+
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+  return res.json();
+}
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
-  const headers = { Accept: "application/json" };
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
   const [status, setStatus] = useState("");
+  useEffect(() => {
+    if (data) {
+      setStatus(data.description);
+    }
+  }, [data])
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
-
+  if (error) return <><p>Failed to load.</p></>;
+  if (isLoading) return <><p>Loading...</p></>;
   return <>{status && <p>Status : {status}</p>}</>;
 }
 
 export default App;
+
+
+// まずはSWRを使う
